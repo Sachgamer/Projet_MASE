e21d1ac79912fa5069bfc72ca18596ddab85274d
Sachgamer <s.degroote@goron-systemes.fr>
2026-06-24 11:31:57 +0200

    géolocalisation

--- frontend/components/views/ReportCreateView.tsx
+++ frontend/components/views/ReportCreateView.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { createReport } from '@/lib/api';
 import { useView } from '@/context/ViewContext';
 import { Button } from '@/components/ui/button';
